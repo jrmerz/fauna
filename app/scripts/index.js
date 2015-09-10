3e067665952ea2beb7cad2927a2f2a0227a3d792
Justin Merz <jrmerz@gmail.com>
2015-09-10 15:36:13 -0700

    adding new image assest, adding buy btn, adding schema.org props

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,20 @@
-var FC = {};
+var FC = {
+  listeners : [],
+  addDescriptionLoadHandler : function(callback) {
+    if( this.descriptions ) {
+      return callback();
+    }
+    this.listeners.push(callback);
+  },
+  onDescriptionsLoad : function() {
+    for( var i = 0; i < this.listeners.length; i++ ) {
+      this.listeners[i]();
+    }
+    this.listeners = null;
+  }
+};
+
+$.get('/description.json', function(resp){
+  FC.descriptions = resp;
+  FC.onDescriptionsLoad();
+}.bind(this));
